f4a91fc6a585567c0cf70135800cb526a69e3638
William Dizon <wdchromium@gmail.com>
2021-07-18 13:21:56 -0700

    wheel and buttons functional to drive

---
+++ wheel.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+# Author: William Dizon <wdchromium@gmail.com>
+
+import asyncio
+import logging
+
+logger = logging.getLogger(__name__)
+
+import sys
+sys.path.insert(0, "./joycontrol")
+
+# switch controller imports
+from joycontrol import logging_default as log, utils
+from joycontrol.command_line_interface import ControllerCLI
+from joycontrol.controller import Controller
+from joycontrol.controller_state import ControllerState, button_push, button_press, button_release
+from joycontrol.memory import FlashMemory
+from joycontrol.protocol import controller_protocol_factory
+from joycontrol.server import create_hid_server
+
+# devices
+from fcntl import ioctl
+from evdev import InputDevice, categorize, ecodes
+wheel_buttons = InputDevice('/dev/input/event1')
+wheel_steering = open('/dev/input/js0', 'rb')
+
+import array, struct
+
+# Get the device name.
+buf = array.array('B', [0] * 64)
+ioctl(wheel_steering, 0x80006a13 + (0x10000 * len(buf)), buf) # JSIOCGNAME(len)
+js_name = buf.tobytes().rstrip(b'\x00').decode('utf-8')
+print(('Device name: %s' % js_name))
+
+async def buttons(device, controller_state, cli):
+    async for ev in device.async_read_loop():
+        #print(repr(ev))
+        if ev.type == 1: # buttons
+            if ev.code == 314:
+                if ev.value: # left button push down 
+                    await asyncio.create_task(button_press(controller_state, 'a'))
+                elif not ev.value: # left button return up
+                    await asyncio.create_task(button_release(controller_state, 'b'))
+            if ev.code == 315:
+                if ev.value: # right button push down 
+                    await asyncio.create_task(button_press(controller_state, 'a'))
+                elif not ev.value: # right button return up
+                    await asyncio.create_task(button_release(controller_state, 'a'))
+        if ev.type == 3: # wheel
+            await asyncio.create_task(cli.cmd_stick('l', 'horizontal', ev.value))
+
+async def _main():
+    # Get controller name to emulate from arguments
+    controller = Controller.from_arg('PRO_CONTROLLER')
+
+    # prepare the the emulated controller
+    factory = controller_protocol_factory(controller,
+                                          spi_flash=FlashMemory(),
+                                          reconnect = None)
+    ctl_psm, itr_psm = 17, 19
+    transport, protocol = await create_hid_server(factory,
+                                                  reconnect_bt_addr="B8:8A:EC:FD:C7:F5",
+                                                  ctl_psm=ctl_psm,
+                                                  itr_psm=itr_psm,
+                                                  device_id=None,
+                                                  interactive=True)
+
+    controller_state = protocol.get_controller_state()
+    c_cli = ControllerCLI(controller_state)
+
+    while True:
+        await buttons(wheel_buttons, controller_state, c_cli)
+
+if __name__ == '__main__':
+    log.configure()
+    loop = asyncio.get_event_loop()
+    asyncio.run(_main())
